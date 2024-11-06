78249fe4c151964da42cd173372f276884ccedef
hyeon8571 <w01085914442@gmail.com>
2024-11-06 13:35:36 +0900

    Fix/#29: 시큐리티 권한 검사 추가

--- BE/src/main/java/com/ottogi/be/auth/config/SecurityConfig.java
+++ BE/src/main/java/com/ottogi/be/auth/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ottogi.be.auth.config;
 import com.ottogi.be.auth.filter.AuthenticationFilter;
 import com.ottogi.be.auth.filter.AuthorizationFilter;
 import com.ottogi.be.auth.filter.CustomLogoutFilter;
+import com.ottogi.be.auth.handler.CustomAccessDeniedHandler;
 import com.ottogi.be.auth.service.JwtService;
 import com.ottogi.be.auth.util.JwtUtil;
 import com.ottogi.be.common.infra.RedisService;
@@ -26,6 +27,7 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CustomAccessDeniedHandler accessDeniedHandler;
     private final AuthenticationConfiguration authenticationConfiguration;
     private final MemberRepository memberRepository;
     private final JwtService jwtService;
@@ -55,7 +57,11 @@ public class SecurityConfig {
                         .requestMatchers("/auth/login", "/members").permitAll()
                         .requestMatchers("/auth/refresh").permitAll()
                         .requestMatchers("/auth/phone/*", "/members/check-nickname", "/members/check-phone", "/members/check-id").permitAll()
-                        .anyRequest().authenticated());
+                        //.requestMatchers("/chat/**").hasRole("OWNER")
+                        .anyRequest().authenticated())
+                .exceptionHandling(exceptions -> exceptions
+                        .accessDeniedHandler(accessDeniedHandler)
+                );
         http
                 .addFilterAt(new AuthenticationFilter(authenticationManager(authenticationConfiguration), jwtService, jwtUtil), UsernamePasswordAuthenticationFilter.class);
         http
