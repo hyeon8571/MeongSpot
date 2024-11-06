2b15b54f02edb50379e813f16c68c37f1de91e01
suwhan2 <jerry5230@naver.com>
2024-11-06 21:00:27 +0900

    Feat/#24: 레디스 관리 테스트

--- gps/src/main/java/com/ottogi/gps/walking/config/WebSocketConfig.java
+++ gps/src/main/java/com/ottogi/gps/walking/config/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(walkingSessionHandler, "/gps").setAllowedOrigins("*");
+        registry.addHandler(walkingSessionHandler, "/ws/location").setAllowedOrigins("*");
     }
 }
--- gps/src/main/java/com/ottogi/gps/walking/repository/WalkingRedisRepository.java
+++ gps/src/main/java/com/ottogi/gps/walking/repository/WalkingRedisRepository.java
@@ -13,17 +13,7 @@ public class WalkingRedisRepository {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public void saveStartTime(Long userId, Long startTime) {
-        String key = "walking:" + userId + ":startTime";
-        redisTemplate.opsForValue().set(key, startTime);
-    }
-
-    public void saveDogIds(Long userId, List<Long> dogIds) {
-        String key = "walking:" + userId + ":dogIds";
-        redisTemplate.opsForList().rightPushAll(key, dogIds);
-    }
-
-    public void saveGpsData(Long userId, BigDecimal lat, BigDecimal lng) {
+    public void saveGpsData(Long userId, double lat, double lng) {
         String key = "walking:" + userId + ":gps";
         redisTemplate.opsForList().rightPush(key, lat + "," + lng);
     }
