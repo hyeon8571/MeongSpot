fa80e6cb40e818dbccf6d7b903b6aa9130985b58
hyeon8571 <w01085914442@gmail.com>
2024-11-10 01:54:18 +0900

    Fix/#53: 채팅방 관련 기능 수정

--- BE/src/main/java/com/ottogi/be/meeting/domain/Meeting.java
+++ BE/src/main/java/com/ottogi/be/meeting/domain/Meeting.java
@@ -13,7 +13,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public class Meeting {
     @Id
