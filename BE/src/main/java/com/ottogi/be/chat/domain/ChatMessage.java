87101560f0e4690b871706d6aa03965f118aea69
hyeon8571 <w01085914442@gmail.com>
2024-11-10 10:12:55 +0900

    Feat/#54: 모임 채팅방 처음 입장 시 입장 메시지 전송

--- BE/src/main/java/com/ottogi/be/chat/domain/ChatMessage.java
+++ BE/src/main/java/com/ottogi/be/chat/domain/ChatMessage.java
@@ -3,6 +3,7 @@ package com.ottogi.be.chat.domain;
 import com.ottogi.be.chat.domain.enums.MessageType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @Document(collection = "message")
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessage {
