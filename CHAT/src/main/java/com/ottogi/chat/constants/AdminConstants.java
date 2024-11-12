f3f65f047e2e29edb513e5cb958522aa9fd0296a
hyeon8571 <w01085914442@gmail.com>
2024-11-12 16:13:19 +0900

    Fix/#76: 채팅방 입장 메시지 전송

---
+++ CHAT/src/main/java/com/ottogi/chat/constants/AdminConstants.java
@@ -0,0 +1,5 @@
+package com.ottogi.chat.constants;
+
+public class AdminConstants {
+    public static final Long ADMIN_ID = 1L;
+}
---
+++ CHAT/src/main/java/com/ottogi/chat/dto/EnterChatRoomDto.java
@@ -0,0 +1,12 @@
+package com.ottogi.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnterChatRoomDto {
+    private String nickname;
+}
